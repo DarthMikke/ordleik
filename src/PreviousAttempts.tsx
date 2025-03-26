8dce6133e53678b3a25040482b32af6780d9f271
Michal Jan Warecki <mwarecki1@gmail.com>
2025-03-26 23:52:16 +0100

    WIP: Show attempts in a nicer way.

--- src/PreviousAttempts.tsx
+++ src/PreviousAttempts.tsx
@@ -1,16 +1,40 @@
 import { useContext } from "react"
 import { gameContext } from "./gameContext"
-import { validate } from "./validate"
+import { SolutionValidity, validate } from "./validate"
+import './PreviousAttempts.css'
 
-function SingleAttempt({attempt, ...props}: {attempt: string} & React.PropsWithChildren) {
-	const { solution } = useContext(gameContext);
+function SingleAttempt({attempt, index, ...props}: {attempt: string, index: number} & React.PropsWithChildren) {
+	const { solution, attempts } = useContext(gameContext);
+	const validityMapping = validate(attempt, solution);
 
-	return <p {...props}>{attempt}</p>
+	if (attempt.length < 5) {
+		attempt = attempt.padEnd(5, ' ')
+	}
+
+	return <p className="single-attempt"
+		id={`single-attempt-${index}`}
+		style={{bottom: (attempts.length-index)*42 + 'px'}}
+		{...props}>{Array.from(attempt).map((char, i) => {
+		const cname = (() => {
+			switch (validityMapping[i]) {
+			case SolutionValidity.Incorrect:
+				return "letter-incorrect";
+			case SolutionValidity.WrongPlacement:
+				return "letter-wrong_placement";
+			case SolutionValidity.Correct:
+				return "letter-correct";
+			default:
+				return "";
+			}
+		})();
+		return <span key={`char-${i}`} className={`letter ${cname}`}
+			>{char}</span>
+	})}</p>
 }
 
 export function PreviousAttempts() {
 	const { attempts } = useContext(gameContext);
-	return <div>
-      {attempts.map((x, i) => <SingleAttempt key={i} attempt={x}/>)}
+	return <div className="previous-attempts">
+      {attempts.map((x, i) => <SingleAttempt key={i} index={i} attempt={x}/>)}
     </div>
 }
