6d90c8550c42b6f2e146ca3601e1b1fee9af23e2
Michal Jan Warecki <mwarecki1@gmail.com>
2025-03-31 23:57:20 +0200

    Multiple words.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,47 @@
+import {
+  createHashRouter,
+  redirect,
+  RouterProvider,
+  useLoaderData,
+} from "react-router";
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+const wordList = [
+  'water',
+  'otter',
+  'hound',
+  'pizza',
+  'eagle',
+  'fruit',
+  'paper',
+];
+
+const Root = () => {useLoaderData<number>();
+  return <></>;
+}
+
+const router = createHashRouter([
+  {
+    path: "/",
+    element: <Root />,
+    loader: (() => {
+      const gameId = Math.floor(Math.random()*(wordList.length - 1));
+      console.debug("Redirecting to ", gameId);
+      return redirect(`${gameId}`);
+    })
+  },
+  {
+    path: "/:word",
+    element: <App />,
+  },
+]);
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
