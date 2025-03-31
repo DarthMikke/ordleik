ea6b4daed1703dead49acd22846e9eb64fff720e
Michal Jan Warecki <mwarecki1@gmail.com>
2025-03-31 23:27:26 +0200

    Working validation function.

--- src/validate.ts
+++ src/validate.ts
@@ -5,21 +5,32 @@ export enum SolutionValidity {
 }
 
 export function validate(attempt: string, solution: string) {
-	const mapping: SolutionValidity[] = [];
+	let _solution = Array.from(solution).map(
+		(x, i) => {return {letter: x, index: i}}
+	)
 
-	for (var i = 0; i < Math.min(attempt.length, solution.length); i++) {
-		if (attempt[i] == solution[i]) {
-			mapping.push(SolutionValidity.Correct);
-			continue;
+	let ret = [..._solution].map((x) => {
+		// Happy path: The inserted letter matches the solution
+		if (x.letter == attempt[x.index]) {
+			_solution = _solution.filter(y => x.index != y.index);
+			return SolutionValidity.Correct;
+		} else {
+			return SolutionValidity.Incorrect;
+		}
+	});
+
+	return ret.map((x, i) => {
+		if (x == SolutionValidity.Correct) {
+			return x;
 		}
 
-		if (solution.includes(attempt[i])) {
-			mapping.push(SolutionValidity.WrongPlacement);
-			continue;
+		// Second best: The inserted letter is somewhere in the solution
+		const letterIndex = _solution.findIndex(y => y.letter == attempt[i]);
+		if (letterIndex >= 0) {
+			_solution = _solution.filter(y => _solution[letterIndex].index != y.index);
+			return SolutionValidity.WrongPlacement;
 		}
 
-		mapping.push(SolutionValidity.Incorrect);
-	}
-
-	return mapping;
+		return SolutionValidity.Incorrect;
+	});
 }
